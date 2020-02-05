c3d3ddb44c4d1e67b0a02e55d916ae15f0bb84db
chaitrabhandiwad <chaitrabhandiwad@users.noreply.github.com>
2020-02-05 18:02:42 +0100

    Resubmission of server.js file

---
+++ server.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+/* eslint-env commonjs */
+
+const http = require('http'),
+  fs = require('fs'),
+  url = require('url');
+
+http.createServer((request, response) => {
+  var addr = request.url,
+    q = url.parse(addr, true),
+    filePath = '';
+
+  if (q.pathname.includes('documentation')) {
+    filePath = 'documentation.html';
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.');
+    }
+  });
+
+}).listen(8080);
