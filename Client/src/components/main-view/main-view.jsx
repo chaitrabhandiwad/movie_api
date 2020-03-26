057c630bc43d1d0b38b4c64b4fd380ec0be15a39
chaitrabhandiwad <chaitrabhandiwad@ymail.com>
2020-03-26 23:40:19 +0100

    Excercise 3.4 :Adding Prop-type and react bootstrap
    Also added two new views login and registration and designed them with bootstrap react.

--- Client/src/components/main-view/main-view.jsx
+++ Client/src/components/main-view/main-view.jsx
@@ -1,22 +1,41 @@
+// client/src/main-view/main-view.jsx
 import React from 'react';
 import axios from 'axios';
+
+// views imports
+import { LoginView } from '../Login-view/Login-view';
+import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
+// react-bootstrap imports
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Navbar from 'react-bootstrap/Navbar';
+import './main-view.scss';
+
+
+
 export class MainView extends React.Component {
+
     constructor() {
         //Call the superclass constructor
         // so React can initialize it
         super();
 
         // Initialize the state to an empty object so we can destructure it later
+
         this.state = {
             movie: null,
-            selectedMovie: null
+            selectedMovie: null,
+            user: null,
+            newUser: false
         };
     }
 
     //One fot the "hooks" available in a react component
+
     componentDidMount() {
         axios
             .get('https://ancient-mullhouse.herokuapp.com/movies')
@@ -36,37 +55,99 @@ export class MainView extends React.Component {
         });
     }
 
+
+
     onBackClick(movie) {
+
         this.setState({
             selectedMovie: null
+        });
+    }
+
+    onLoggedIn(user) {
+        this.setState({
+            user
+        });
+    }
+
+    newUser() {
+        this.setState({
+            newUser: true
+        });
+    }
+    onSignedIn(user) {
+        this.setState({
+            user: user,
+            register: false
+        });
+    }
+
+    alreadyRegistered() {
+        this.setState({
+            newUser: false
         });
     }
 
     render() {
         // If the state isn't initialized, this will throw on runtime
         // before the data is initially loaded
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user, newUser } = this.state;
 
-        //Before the movies have been loaded
-        if (!movies) return <div className='main-view' />;
+        if (!user && newUser === false)
+            return (
+                <LoginView
+                    onClick={() => this.newUser()}
+                    onLoggedIn={user => this.onLoggedIn(user)}
+                />
+            );
+
+        if (newUser)
+            return (
+                <RegistrationView
+                    onClick={() => this.alreadyRegistered()}
+                    onSignedIn={user => this.onSignedIn(user)}
+                />
+            );
+
+        // Before the movies have been loaded //change the class to avoid confusion
+        // if (!movies) return <div className='main-view' />;
+
+        if (!movies) return <body className='mainV' />;
 
         return (
-            <div className='main-view'>
-                {selectedMovie ? (
-                    <MovieView
-                        movie={selectedMovie}
-                        onClick={button => this.onBackClick()}
-                    />
-                ) : (
-                        movies.map(movie => (
-                            <MovieCard
-                                key={movie._id}
-                                movie={movie}
-                                onClick={movie => this.onMovieClick(movie)}
-                            />
-                        ))
-                    )}
-            </div>
+            <body className='mainV'>
+                <Navbar className='title' fluid='true'>
+                    <Navbar.Brand className='t'>
+                        Welcome to my Movie App
+          </Navbar.Brand>
+                </Navbar>
+
+                <Container className='main-view'>
+                    <Row>
+                        {selectedMovie ? (
+                            <Col>
+                                <MovieView
+                                    movie={selectedMovie}
+                                    onClick={button => this.onBackClick()}
+                                />
+                            </Col>
+                        ) : (
+                                movies.map(movie => (
+                                    <Col xl={4} sm={6} md={4} xs={10}>
+                                        <MovieCard
+                                            key={movie._id}
+                                            movie={movie}
+                                            onClick={movie => this.onMovieClick(movie)}
+                                        />
+                                    </Col>
+                                ))
+                            )}
+                    </Row>
+                </Container>
+                <Navbar fixed='bottom' className='borderBottom' fluid='true'>
+                    <Navbar.Brand className='t'></Navbar.Brand>
+                </Navbar>
+            </body>
         );
     }
 }
--- Client/src/components/movie-view/movie-view.jsx
+++ Client/src/components/movie-view/movie-view.jsx
@@ -1,40 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import './movie-view.scss';
 
 export class MovieView extends React.Component {
+
     constructor() {
         super();
-
         this.state = {};
     }
+
     render() {
         const { movie, onClick } = this.props;
 
         if (!movie) return null;
 
-        return (<div className="movie-view">
-            <img className="movie-poster" src={movie.ImagePath} />
-            <div className="movie-title">
-                <span className="label">Title: </span>
-                <span className="value">{movie.Title}</span>
-            </div>
-            <div className="movie-description">
-                <span className="label">Description: </span>
-                <span className="value">{movie.Description}</span>
-            </div>
+        return (
+            <div className='movie-view'>
+                <div className='movie-title'>
+                    <h4 className='label'>Title</h4>
+                    <p className='value'>{movie.Title}</p>
+                </div>
 
-            <div className="movie-genre">
-                <span className="label">Genre: </span>
-                <span className="value">{movie.Genre.Name}</span>
-            </div>
-            <div className="movie-director">
-                <span className="label">Director: </span>
-                <span className="value">{movie.Director.Name}</span>
-            </div>
-            <button onClick={() => onClick()}>Back</button>
-        </div>
+                <div className='movie-description'>
+                    <h4 className='label'>Description</h4>
+                    <p className='value'>{movie.Description}</p>
+                </div>
 
+                <img className='movie-poster' src={movie.ImagePath} />
 
+                <div className='movie-genre'>
+                    <h4 className='label'>Genre</h4>
+                    <p className='value'>{movie.Genre.Name}</p>
+                </div>
+                <div className='movie-director'>
+                    <h4 className='label'>Director</h4>
+                    <p className='value'>{movie.Director.Name}</p>
+                </div>
+                <button onClick={() => onClick()}>Back</button>
+            </div>
         );
-
     }
 }
+
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string
+    }).isRequired,
+    onClick: PropTypes.func.isRequired
+};
